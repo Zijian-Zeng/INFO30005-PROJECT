d3159cc3e3686ca2fff01dfbefee9d08499e10b7
ReginaLin24 <rylin@student.unimelb.edu.au>
2020-05-03 18:34:20 +1000

    meetute v0.4

--- server.js
+++ server.js
@@ -12,38 +12,43 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//Connect to database.
 require("./Models/db");
 
+//Logger.
 app.use(morgan("common"));
 
+//Allow access from anywhere.
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-	);
-	next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    next();
 });
 
+//Back-end APIs.
 app.use("/api", require("./routes/api/index"));
 
+//Front-end APIs.
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-	});
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 }
 
-//port setup
+//Port setup.
 app.set("port", process.env.PORT || 5000);
 app.listen(app.get("port"), function () {
-	console.log(
-		"Express started on http://localhost:" +
-			app.get("port") +
-			"; press Ctrl-C to terminate."
-	);
+    console.log(
+        "Express started on http://localhost:" +
+            app.get("port") +
+            "; press Ctrl-C to terminate."
+    );
 });
 
 module.exports = app;
