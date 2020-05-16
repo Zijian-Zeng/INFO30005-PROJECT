dcdfe8f92025b6f4f23e8283613e279e3af66e3f
Jamie991014 <62122147+Jamie991014@users.noreply.github.com>
2020-05-16 18:10:41 +1000

    fix student test

--- client/src/component/Questionboard/Student copy.jsx
+++ client/src/component/Questionboard/Student copy.jsx
@@ -15,6 +15,7 @@ import {
 	ListItemText,
 	Divider,
 	Badge,
+	Fab,
 } from "@material-ui/core";
 
 import FolderIcon from "@material-ui/icons/Folder";
@@ -26,7 +27,7 @@ import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
 import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 
-import { myFetch, UserContext, StaffContext } from "../Methods";
+import { myFetch, UserContext, StudentContext } from "../Methods";
 import Staff from "./Staff";
 const style = ({ palette, spacing }) => ({
 	icon: {
@@ -46,93 +47,43 @@ const style = ({ palette, spacing }) => ({
 	left: {
 		marginLeft: spacing(1),
 	},
-	icon: {
-		color: palette.action.active,
-		marginLeft: spacing(1),
-	},
-	textCenter: {
-		textAlign: "center",
-	},
-	header: {
-		height: "260px",
-		backgroundSize: "cover",
-	},
-	commandButton: {
-		backgroundColor: "rgba(255,255,100,0.65)",
-	},
-	left: {
-		marginLeft: spacing(1),
-	},icon: {
-		color: palette.action.active,
-		marginLeft: spacing(1),
-	},
-	textCenter: {
-		textAlign: "center",
-	},
-	header: {
-		height: "260px",
-		backgroundSize: "cover",
-	},
-	commandButton: {
-		backgroundColor: "rgba(255,255,100,0.65)",
-	},
-	left: {
-		marginLeft: spacing(1),
-	},icon: {
-		color: palette.action.active,
-		marginLeft: spacing(1),
-	},
-	textCenter: {
-		textAlign: "center",
-	},
-	header: {
-		height: "260px",
-		backgroundSize: "cover",
-	},
-	commandButton: {
-		backgroundColor: "rgba(255,255,100,0.65)",
-	},
-	left: {
-		marginLeft: spacing(1),
-	},icon: {
-		color: palette.action.active,
-		marginLeft: spacing(1),
-	},
-	textCenter: {
-		textAlign: "center",
-	},
-	header: {
-		height: "260px",
-		backgroundSize: "cover",
-	},
-	commandButton: {
-		backgroundColor: "rgba(255,255,100,0.65)",
-	},
-	left: {
-		marginLeft: spacing(1),
-	},icon: {
-		color: palette.action.active,
-		marginLeft: spacing(1),
-	},
-	textCenter: {
-		textAlign: "center",
-	},
-	header: {
-		height: "260px",
-		backgroundSize: "cover",
-	},
-	commandButton: {
-		backgroundColor: "rgba(255,255,100,0.65)",
-	},
-	left: {
-		marginLeft: spacing(1),
+	fab: {
+		margin: "auto",
+		width: "100%",
 	},
 });
 
 export default withStyles(style, { name: "Content" })(
 	({ children, appointmentData, classes, ...restProps }) => {
 		//const {} = useContext(StaffContext);
-		console.log(appointmentData);
+		const { setBookOpen, setLoading, userInfo, setUser } = useContext(
+			StudentContext
+		);
+		const { detectAlert, setLoadingRoute } = useContext(UserContext);
+
+		const book = async () => {
+			setLoading(true);
+			const body = {
+				id: appointmentData.id,
+			};
+			const res = await myFetch(
+				"/api/student/consult/book",
+				"POST",
+				body
+			);
+			detectAlert(res, "Successfully booked.");
+		};
+
+		const checkBooked = () => {
+			console.log(userInfo.registeredConsult);
+			console.log(appointmentData.id);
+			return (
+				userInfo.registeredConsult.filter(
+					(each) => each == appointmentData.id
+				).length > 0
+			);
+		};
+
 		return (
 			<AppointmentTooltip.Content
 				{...restProps}
@@ -150,6 +101,7 @@ export default withStyles(style, { name: "Content" })(
 									secondary={appointmentData.location}
 								/>
 							</ListItem>
+
 							<ListItem>
 								<ListItemIcon>
 									<SupervisorAccountIcon />
@@ -191,6 +143,21 @@ export default withStyles(style, { name: "Content" })(
 										<EqualizerIcon />
 									</Badge>
 								</ListItemIcon>
+							</ListItem>
+							<br />
+							<ListItem>
+								<Fab
+									variant="extended"
+									className={classes.fab}
+									color={
+										checkBooked() ? "default" : "primary"
+									}
+									onClick={book}
+									disabled={checkBooked()}
+								>
+									<PlaylistAddIcon />
+									{checkBooked() ? "Booked" : "Book it"}
+								</Fab>
 							</ListItem>
 						</List>
 					</Grid>
