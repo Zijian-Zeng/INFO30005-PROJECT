73a6ae5024d472333939592256a60e1f39db9866
queloibtskts <queloibtskts@users.noreply.github.com>
2020-05-16 12:15:36 +1000

    change timetable

--- client/src/component/Test/TimeTable.jsx
+++ client/src/component/Test/TimeTable.jsx
@@ -55,6 +55,96 @@ const styles = (theme) => ({
     ...theme.typography.h6,
     marginRight: theme.spacing(2),
   },
+  paper: {
+    maxHeight: "70VH",
+  },
+  fab: {
+    position: "fixed",
+    bottom: theme.spacing(1) * 6,
+    right: theme.spacing(1) * 6,
+  },
+  noDecoration: {
+    textDecoration: "none !important",
+  },
+  delete: {
+    textTransform: "none",
+    color: theme.palette.getContrastText(grey[700]),
+    background: grey[700],
+    "&:hover": {
+      color: theme.palette.getContrastText(grey[900]),
+      background: grey[900],
+    },
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  approve: {
+    backgroundColor: green[400],
+    "&:hover": {
+      backgroundColor: green[600],
+    },
+  },
+  decline: {
+    backgroundColor: red[500],
+    "&:hover": {
+      backgroundColor: red[700],
+    },
+  },
+  container: {
+    width: theme.spacing(68),
+    padding: 0,
+    paddingBottom: theme.spacing(2),
+  },
+  content: {
+    width: theme.spacing(68),
+    padding: 0,
+    paddingBottom: theme.spacing(2),
+  },
+  header: {
+    overflow: "hidden",
+    paddingTop: theme.spacing(0.5),
+  },
+  buttonGroup: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 2),
+  },
+  closeButton: {
+    float: "right",
+  },
+  clickButton: {
+    marginLeft: theme.spacing(2),
+  },
+  picker: {
+    marginRight: theme.spacing(2),
+    "&:last-child": {
+      marginRight: 0,
+    },
+    width: "50%",
+  },
+  wrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: theme.spacing(1, 0),
+  },
+  textField: {
+    width: "100%",
+  },
+  approve: {
+    backgroundColor: green[400],
+    "&:hover": {
+      backgroundColor: green[600],
+    },
+  },
+  decline: {
+    backgroundColor: red[500],
+    "&:hover": {
+      backgroundColor: red[700],
+    },
+  },
 });
 
 const ToolbarWithLoading = withStyles(
